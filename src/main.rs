7afed48e253c771b6b494aa7c3554dd36c9db6c3
Janusz Piechówka <januszpiechowka@gmail.com>
2024-01-11 16:29:59 +0100

    feat: add application icon and update dependencies
    - an icon has been added to the application through the new 'icon.rc' file and build.rs script
    - dependencies have been updated in the Cargo.toml file to include 'embed-resource', 'image' and 'winit'
    - README.md file has been updated with information regarding asset and icon creation
    - the application's main.rs file now includes a system to set the window icon

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use bevy::prelude::*;
-use bevy::window::{PresentMode, WindowTheme};
+use std::io::Cursor;
+#[cfg(feature = "debug")]
 use std::time::Duration;
 
 #[cfg(feature = "debug")]
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
-
+use bevy::prelude::*;
+use bevy::window::{PresentMode, PrimaryWindow, WindowTheme};
+use bevy::winit::WinitWindows;
 #[cfg(feature = "debug")]
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use winit::window::Icon;
 
 const WINDOW_TITLE: &str = "3D Cellular Automata";
 const INITIAL_RESOLUTION_X: u16 = 1280;
@@ -37,7 +40,30 @@ fn main() {
     #[cfg(feature = "debug")]
     add_debug_plugins(&mut app);
 
+    app.add_systems(Startup, set_window_icon);
     app.run();
+}
+
+fn set_window_icon(
+    windows: NonSend<WinitWindows>,
+    primary_window: Query<Entity, With<PrimaryWindow>>,
+) {
+    info!("Setting the window icon");
+    let primary_entity = primary_window.single();
+
+    let Some(primary) = windows.get_window(primary_entity) else {
+        return;
+    };
+
+    let icon_buf = Cursor::new(include_bytes!("../icons/C-Icon-256.png"));
+
+    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
+        let image = image.into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        let icon = Icon::from_rgba(rgba, width, height).unwrap();
+        primary.set_window_icon(Some(icon));
+    };
 }
 
 #[cfg(feature = "debug")]
